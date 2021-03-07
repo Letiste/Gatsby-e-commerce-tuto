a687a03220730337ae4b2d624727a24b389565b7
Léo Salé <leo.sale72@gmail.com>
2021-03-07 14:56:49 +0100

    Add Add to Cart button

---
+++ src/pages/index.tsx
@@ -0,0 +1,68 @@
+import * as React from "react"
+import { Link, graphql, useStaticQuery } from "gatsby"
+import { GatsbyImage, getImage, IGatsbyImageData } from "gatsby-plugin-image"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+import products from "../products.json"
+
+const IndexPage = ({ data }) => {
+  // console.log(data.allImageSharp.nodes)
+  const images: Array<IGatsbyImageData | undefined> = []
+  data.allFile.edges.forEach(({ node }) => {
+    images.push(getImage(node))
+  })
+  const imagesName: String[] = []
+  data.allFile.edges.forEach(({ node }) => {
+    imagesName.push(node.name)
+  })
+
+  return (
+    <Layout>
+      <SEO title="Home" />
+      <h1>Home</h1>
+      {products.map(product => {
+        const imageIndex = imagesName.indexOf(product.image)
+        const image = images[imageIndex]
+        return (
+          <div key={product.id} className="">
+            <h3>{product.title}</h3>
+            <GatsbyImage image={image!} alt={`Preview of ${product.title}`} />
+            <p>{product.description}</p>
+            <p>${product.price}</p>
+            <p>
+              <button
+                className="snipcart-add-item"
+                data-item-id={product.id}
+                data-item-image={image}
+                data-item-name={product.title}
+                data-item-url="/"
+                data-item-price={product.price}
+              >
+                Add to Cart
+              </button>
+            </p>
+          </div>
+        )
+      })}
+    </Layout>
+  )
+}
+
+export default IndexPage
+
+export const data = graphql`
+  query MyQuery {
+    allFile(filter: { relativeDirectory: { eq: "products" } }) {
+      edges {
+        node {
+          childImageSharp {
+            gatsbyImageData(placeholder: BLURRED, width: 200)
+          }
+          name
+        }
+      }
+    }
+  }
+`
